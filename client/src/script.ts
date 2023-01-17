ef4889b5b92ee298fbaf64de57f92978a4b9b48f
Danilson Reis gh <danyreys47@gmail.com>
2023-01-17 17:17:54 -0100

    Using production server

--- client/src/script.ts
+++ client/src/script.ts
@@ -74,7 +74,7 @@ const handleSubmit = async (event: Event) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://dreis-codex.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
